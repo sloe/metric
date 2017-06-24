42b07314ea2079dcf08856f0e5029910834fa753
sloe <sloe@sloecoach.com>
2017-06-24 19:24:23 +0100

    YouTube control test elements.

--- static/mtjs/mtyoutube.js
+++ static/mtjs/mtyoutube.js
@@ -7,6 +7,7 @@ function MtYouTubePlayer () {
     this.player = new YT.Player(divname, {
       height: '390',
       width: '640',
+      playerVars: {color: 'red', modestbranding: 1, rel: 0, start: 60},
       videoId: videoId,
       events: {
         'onReady': this.onPlayerReady,
@@ -25,5 +26,22 @@ function MtYouTubePlayer () {
 
   this.stopVideo = function() {
     this.player.stopVideo();
+  };
+
+  this.doPause = function() {
+    this.player.pauseVideo();
+  };
+
+  this.doSeek = function(seconds) {
+    this.player.seekTo(seconds);
+  };
+
+  this.setSize = function(width, height) {
+    this.player.setSize(width, height);
+  };
+
+  this.doNudge = function(offset) {
+    var timeNow = this.player.getCurrentTime();
+    this.player.seekTo(timeNow + offset, true);
   };
 };
